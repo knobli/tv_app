b4c7df0dc0d908623f754581c9aab07029fdcd5f
Raffael Santschi <raffael.santschi@inacta.ch>
2022-01-24 11:35:23 +0100

    Change release build and setup env, change config, move icons, remove node_modules

--- android_release_build.sh
+++ android_release_build.sh
@@ -1,3 +1,8 @@
-phonegap build android
-rm -r platforms/android/assets/www/res 
-platforms/android/cordova/build --release
+#phonegap build android
+#rm -r platforms/android/assets/www/res 
+#platforms/android/cordova/build --release
+
+
+cordova build --release android
+jarsigner.exe -keystore ~/.ssh/tv-app-key.keystore -sigalg SHA1withRSA -digestalg SHA1 platforms/android/build/outputs/apk/android-release-unsigned.apk tv_app
+/c/Users/inacta/AppData/Local/Android/Sdk/build-tools/31.0.0/zipalign.exe -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/tv-grafstal.apk
---
+++ android_setup_env.bat
@@ -0,0 +1,4 @@
+set ANDROID_HOME=C:\Users\inacta\AppData\Local\Android\Sdk
+set PATH=%PATH%;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools
+
+Open Android SDK, create new empty Project, Tools -> AVW Manager -> Create new Device, download x86_64 os, that's it
